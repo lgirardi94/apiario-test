cdb6ac8054ff3b50e2f3eca5b4f4d6563f92da89
lgirardi94 <lgirardi94@gmail.com>
2026-04-24 14:38:18 +0200

    Update arnie.js

--- js/modules/arnie.js
+++ js/modules/arnie.js
@@ -1,10 +1,7 @@
 import { uuid } from '../utils.js';
 
 export function addArnia(store, nome) {
-  store.data.arnie.push({
-    id: uuid(),
-    nome
-  });
+  store.data.arnie.push({ id: uuid(), nome });
   store.save();
 }
 
